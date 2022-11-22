1fa128706e90c966fa8908fb1af32c7e90cda8eb
まさにょん <78206917+yukimura-manase@users.noreply.github.com>
2022-11-22 18:57:00 +0900

    Create NotFound.js

---
+++ React/NotFound.js
@@ -0,0 +1,14 @@
+
+const notFoundStyle = {
+    textAlign: "center",
+    fontSize: "30px",
+    fontWeight: "bold"
+};
+
+export const NotFound = () => {
+    return (
+        <div style={notFoundStyle}>
+            404 Not Found
+        </div>
+    );
+}
