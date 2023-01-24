49865c9105c0ea8801b068d12f77bb4ee0a490dd
まさにょん <78206917+yukimura-manase@users.noreply.github.com>
2023-01-24 16:04:08 +0900

    Update select_create.php

--- PHP/select_create.php
+++ PHP/select_create.php
@@ -30,39 +30,30 @@ foreach ($data_set as $data) {
     array_push($city_info, $city_obj);
 }
 
-$select_box = '<select id="world-city">';
+
 
 // グループ分けされたSelectBoxを作成する
-foreach ($group_array as $key => $group) {
+foreach ($group_array as $key => $group) { ?>
     
-    $select_box .= "<optgroup label='{$group}'>";
+    <select id="world-city">
+        <optgroup label="<?php echo $group ?>">
+            <?php
+            foreach ($city_info as $city_obj) {
+                if ($city_obj->group_key == $key) { ?>
+                    <option><?php echo $city_obj->city ?></option>
+                <?php
+                }
+            }
+            ?>
+        </optgroup>
+    </select>
+    
+<?php }
 
-    foreach ($city_info as $city_obj) {
-        if ($city_obj->group_key == $key) {
-            $select_box .= "<option>{$city_obj->city}</option>";
-        }
-    }
-    $select_box .= '</optgroup>';
-}
-
-$select_box .= '</select>';
-
-echo $select_box;
 
 
-// <select id="world-city">
-//     <optgroup label='日本'>
-//         <option>東京</option>
-//         <option>大阪</option>
-//     </optgroup>
-//     <optgroup label='ドイツ'>
-//         <option>ベルリン</option>
-//         <option>ミュンヘン</option>
-//     </optgroup>
-//     <optgroup label='アメリカ'>
-//         <option>ニューヨーク</option>
-//         <option>ロサンゼルス</option>
-//     </optgroup>
-// </select>
+
+
+
 
 
